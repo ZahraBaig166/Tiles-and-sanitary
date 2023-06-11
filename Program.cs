afc9fcc7a2eca4432b9e2ee32d62266388f8c56d
ZahraBaig166 <zahrabaig166@gmail.com>
2023-06-11 21:46:29 +0500

    DATA INSERTION

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Data.SqlClient;
+using Bogus;
+
+namespace SupplierDataGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string connectionString = "Data Source=ZAHRA-PC\\SQLEXPRESS;Initial Catalog=TilesandSanitary;Integrated Security=True";
+
+            // Create Faker instance
+            var faker = new Faker();
+
+            // Establish database connection
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                // Call insertion method for Suppliers
+                InsertSupplierData(connection, faker);
+            }
+
+            Console.WriteLine("Data generation completed.");
+            Console.ReadLine();
+        }
+
+        private static void InsertSupplierData(SqlConnection connection, Faker faker)
+        {
+            string insertQuery = "INSERT INTO Supplier (SupplierID, SupplierName, ContactInformation) VALUES (@SupplierID, @SupplierName, @ContactInformation)";
+
+            using (SqlCommand command = new SqlCommand(insertQuery, connection))
+            {
+                for (int i = 1; i <= 150; i++)
+                {
+                    command.Parameters.Clear();
+                    command.Parameters.AddWithValue("@SupplierID", i);
+                    command.Parameters.AddWithValue("@SupplierName", faker.Company.CompanyName());
+                    command.Parameters.AddWithValue("@ContactInformation", "+92 3" + faker.Random.Number(0, 4) + faker.Random.Number(0, 9) + " " + faker.Random.Number(1000000, 9999999));
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+    }
+}
---
+++ SqlConnection.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SANITARY
+{
+    internal class SqlConnection
+    {
+        string username = Environment.UserName;
+        string desktopName = Environment.MachineName;
+
+
+        // Define the connection string to the database
+        string connectionString;
+
+
+        public SqlConnection(String connectionString)
+        {
+            this.connectionString = connectionString;
+        }
+
+        public SqlConnection GetConnection()
+        {
+
+            SqlConnection connection = new SqlConnection(connectionString);
+            connection.GetConnection();
+            return connection;
+        }
+    }
+}
